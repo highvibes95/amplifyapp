bc84800ecbfd652a8697505e21aecb7b96648a64
highvibes95 <30365538+highvibes95@users.noreply.github.com>
2021-01-17 06:31:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       <h2>
+        Hello from amplify aws react 
+    </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
